9d1c449f99433aa3d5b97e106e53d62a1af8a06b
rdevacc <r.devacc@gmail.com>
2025-10-01 19:00:57 +0700

    feat: Adding Link to ANRI and JDIH ANRI in Legal Basis Page

--- resources/views/apps/legal-basis/index.blade.php
+++ resources/views/apps/legal-basis/index.blade.php
@@ -5,6 +5,15 @@
         .min-vh-70 {
             min-height: 70vh;
         }
+
+        .logo-anri img{
+            transition: transform 0.3s ease-in-out;
+        }
+
+        .logo-anri:hover img {
+            transform: scale(1.1);
+        }
+
     </style>
 @endpush
 
@@ -15,6 +24,7 @@
 @section('content')
     <main id="main" class="main">
         <section class="section quantity-unit">
+            
              <!-- Session Alert -->
             @if (session('success'))
                 <div class="alert alert-primary d-flex align-items-center justify-content-between" role="alert">
@@ -27,10 +37,11 @@
                 </div>
             @endif
 
+            <div class="pagetitle">
+                <h1>DASAR HUKUM PELAKSANAAN PENGELOLAAN KEARSIPAN</h1>
+            </div>
+
             <div class="card">
-                <div class="card-header fw-bold">
-                    DASAR HUKUM PELAKSANAAN PENGELOLAAN KEARSIPAN
-                </div>
                 <div class="card-body min-vh-70">
                     <p class="mb-3">
                     Dasar hukum pengelolaan arsip di Balai Besar Perakitan dan Modernisasi Mekanisasi Pertanian, mengikuti dasar hukum sebagai berikut:
@@ -47,6 +58,18 @@
                     <li class="mb-2 ms-4">Perpres Nomor 95 Tahun 2018 tentang Sistem Pemerintahan Berbasis Elektronik (SPBE)</li>
                     <li class="mb-2 ms-4">Perka ANRI Nomor 20/2011 tentang Autentikasi Arsip Elektronik</li>
                     </ol>
+                    <div class="row pt-3 d-flex mx-auto justify-content-center align-items-center text-center gap-3">
+                        <div class="logo-anri col-auto rounded d-flex align-items-center justify-content-center" style="width:150px; height:150px; display:flex; align-items:center; justify-content:center;">
+                            <a href="https://anri.go.id/" target="__blank" rel="noopener noreferrer">
+                                <img src="{{ asset('admin/assets/img/Logo-ANRI.png') }}" alt="Logo-ANRI" style="max-width:100%; max-height:100%; object-fit:contain;">
+                            </a>
+                        </div>
+                        <div class="logo-anri col-auto rounded d-flex align-items-center justify-content-center" style="width:150px; height:150px; display:flex; align-items:center; justify-content:center;">
+                            <a href="https://jdih.anri.go.id/" target="__blank" rel="noopener noreferrer">
+                                <img src="{{ asset('admin/assets/img/Logo-JDIH-ANRI.png') }}" alt="Logo-JDIH-ANRI" style="max-width:100%; max-height:100%; object-fit:contain;">
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
 
